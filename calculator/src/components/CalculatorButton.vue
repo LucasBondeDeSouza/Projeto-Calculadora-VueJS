c47d5ad1c4a56d715ad8efcbc9ae26c1eafd2a36
LucasBondeDeSouza <111398229+LucasBondeDeSouza@users.noreply.github.com>
2023-08-14 15:52:24 -0300

    atualização

--- calculator/src/components/CalculatorButton.vue
+++ calculator/src/components/CalculatorButton.vue
@@ -1,15 +1,55 @@
 <template>
-    <button class="calculator-button">
-        0
+    <button class="calculator-button"
+        @click="$emit('onClick', label)"
+        :class="{ double, triple, operation }">
+        {{ label }}
     </button>
 </template>
 
 <script>
 export default {
-
+    props: {
+        label: {},
+        operation: { type: Boolean },
+        double: { type: Boolean },
+        triple: { type: Boolean }
+    }
 }
 </script>
 
 <style>
+:root {
+    --bg-button: #f0f0f0;
+    --border-button: solid 1px #888;
+}
 
+.calculator-button {
+    font-size: 1.4em;
+    background-color: var(--bg-button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+}
+
+.calculator-button:active {
+    background-color: #ccc;
+}
+
+.calculator-button.double {
+    grid-column: span 2;
+}
+
+.calculator-button.triple {
+    grid-column: span 3;
+}
+
+.calculator-button.operation {
+    background-color: #fa8231;
+    color: #fff;
+}
+
+.calculator-button.operation:active {
+    background-color: #fa8231cc;
+}
 </style>
--- calculator/src/main/CalculatorApp.vue
+++ calculator/src/main/CalculatorApp.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="calculator">
-        <CalculatorDisplay />
-        <CalculatorButton />
+        <CalculatorDisplay :value="displayValue" />
+        <CalculatorButton label="AC" triple @onCLick="clearMemory" />
+        <CalculatorButton label="/" operation @onCLick="setOperation" />
+        <CalculatorButton label="7" @onCLick="addDigit" />
+        <CalculatorButton label="8" @onCLick="addDigit" />
+        <CalculatorButton label="9" @onCLick="addDigit" />
+        <CalculatorButton label="*" operation @onCLick="setOperation" />
+        <CalculatorButton label="4" @onCLick="addDigit" />
+        <CalculatorButton label="5" @onCLick="addDigit" />
+        <CalculatorButton label="6" @onCLick="addDigit" />
+        <CalculatorButton label="-" operation @onCLick="setOperation" />
+        <CalculatorButton label="1" @onCLick="addDigit" />
+        <CalculatorButton label="2" @onCLick="addDigit" />
+        <CalculatorButton label="3" @onCLick="addDigit" />
+        <CalculatorButton label="+" operation @onCLick="setOperation" />
+        <CalculatorButton label="0" double @onCLick="addDigit" />
+        <CalculatorButton label="." @onCLick="addDigit" />
+        <CalculatorButton label="=" operation @onCLick="setOperation" />
     </div>
 </template>
 
@@ -10,7 +26,44 @@ import CalculatorButton from '../components/CalculatorButton.vue';
 import CalculatorDisplay from '../components/CalculatorDisplay.vue';
 
 export default {
-    components: { CalculatorButton, CalculatorDisplay }
+    data: function() {
+        return {
+            displayValue: '0',
+            clearDisplay: false,
+            operation: null,
+            values: [0, 0],
+            current: 0
+        }
+    },
+    components: { CalculatorButton, CalculatorDisplay },
+    methods: {
+        clearMemory() {
+            Object.assign(this.$data, this.$options.data())
+        },
+
+        setOperation(operation) {
+            
+        },
+        
+        addDigit(n) {
+            if (n === "." && this.displayValue.includes(".")) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === "0" || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            if (n !== ".") {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
+        }
+    }
 }
 </script>
 
